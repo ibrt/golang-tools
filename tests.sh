dcdb7c161110937719c14d368d43f725d0c36512
Ivan Bertona <ivan@ibrt.me>
2021-05-10 17:45:24 -0400

    Fix CI.

--- tests.sh
+++ tests.sh
@@ -12,14 +12,14 @@ which golint && (echo 'error: golint is already installed'; exit 1)
 export PATH="$HOME/.gobin:$PATH"
 
 echo "> install go"
-"./tools/govm" install 1.10.1
-[[ -d "$HOME/.govm/go1.10.1" ]] || (echo 'error: go is not installed'; exit 1)
+"./tools/govm" install 1.16.4
+[[ -d "$HOME/.govm/go1.16.4" ]] || (echo 'error: go is not installed'; exit 1)
 
 # activate go
 echo "> activate go"
-eval "$("./tools/govm" activate 1.10.1)"
-[[ "$(which go)" == "$HOME/.govm/go1.10.1/bin/go" ]] || (echo 'error: go is not activated'; exit 1)
-go version | grep "go1.10.1"
+eval "$("./tools/govm" activate 1.16.4)"
+[[ "$(which go)" == "$HOME/.govm/go1.16.4/bin/go" ]] || (echo 'error: go is not activated'; exit 1)
+go version | grep "go1.16.4"
 
 # install golint
 echo "> install golint"
@@ -41,6 +41,6 @@ echo "> uninstall golint"
 [[ ! -f "$HOME/.gobin/golint" ]] || (echo 'error: golint is not uninstalled'; exit 1)
 
 # uninstall go
-echo "> uninstall go 1.10.1"
-"./tools/govm" uninstall 1.10.1
-[[ ! -d "$HOME/.govm/go1.10.1" ]] || (echo 'error: go is not uninstalled'; exit 1)
+echo "> uninstall go 1.16.4"
+"./tools/govm" uninstall 1.16.4
+[[ ! -d "$HOME/.govm/go1.16.4" ]] || (echo 'error: go is not uninstalled'; exit 1)
